3fc48196f501961c1cdd527feedd91664e9ff60b
zafarivaev <z.ivaev@mail.ru>
2019-10-15 16:55:17 +0500

    Beautify the pages and add navigation

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -5,6 +5,8 @@ struct WebsiteController: RouteCollection {
     func boot(router: Router) throws {
         router.get(use: indexHandler)
         router.get("acronyms", Acronym.parameter, use: acronymHandler)
+        router.get("users", User.parameter, use: userHandler)
+        router.get("users", use: allUsersHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
@@ -28,6 +30,28 @@ struct WebsiteController: RouteCollection {
                 }
         }
     }
+    
+    func userHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(User.self)
+            .flatMap(to: View.self) { user in
+                return try user.acronyms
+                .query(on: req)
+                .all()
+                    .flatMap(to: View.self) { acronyms in
+                        let context = UserContext(title: user.name, user: user, acronyms: acronyms)
+                        return try req.view().render("user", context)
+            }
+        }
+    }
+    
+    func allUsersHandler(_ req: Request) throws -> Future<View> {
+        return User.query(on: req)
+        .all()
+            .flatMap(to: View.self) { users in
+                let context = AllUsersContext(title: "All Users", users: users)
+                return try req.view().render("allUsers", context)
+        }
+    }
 }
 
 struct IndexContext: Encodable {
@@ -39,4 +63,15 @@ struct AcronymContext: Encodable {
     let title: String
     let acronym: Acronym
     let user: User
+}
+
+struct UserContext: Encodable {
+    let title: String
+    let user: User
+    let acronyms: [Acronym]
+}
+
+struct AllUsersContext: Encodable {
+    let title: String
+    let users: [User]
 }
