2370903f9c4299d69381e26fd72a55f47add2a19
zafarivaev <z.ivaev@mail.ru>
2019-11-03 19:17:35 +0500

    Add registration with validation

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -17,6 +17,8 @@ struct WebsiteController: RouteCollection {
         authSessionRoutes.get("login", use: loginHandler)
         authSessionRoutes.post(LoginPostData.self, at: "login", use: loginPostHandler)
         authSessionRoutes.post("logout", use: logoutHandler)
+        authSessionRoutes.get("register", use: registerHandler)
+        authSessionRoutes.post(RegisterData.self, at: "register", use: registerPostHandler)
         
         let protectedRoutes = authSessionRoutes.grouped(RedirectMiddleware<User>(path: "/login"))
         protectedRoutes.get("acronyms", "create",
@@ -225,6 +227,36 @@ struct WebsiteController: RouteCollection {
         return req.redirect(to: "/")
     }
     
+    func registerHandler(_ req: Request) throws -> Future<View> {
+        let context: RegisterContext
+        if let message = req.query[String.self, at: "message"] {
+            context = RegisterContext(message: message)
+        } else {
+            context = RegisterContext()
+        }
+        return try req.view().render("register", context)
+    }
+    
+    func registerPostHandler(_ req: Request, data: RegisterData) throws -> Future<Response> {
+        do {
+            try data.validate()
+        } catch (let error){
+            let redirect: String
+            if let error = error as? ValidationError, let message = error.reason.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+                redirect = "/register?message=\(message)"
+            } else {
+                redirect = "/register?message=Unknown+error"
+            }
+            return req.future(req.redirect(to: redirect))
+        }
+        let password = try BCrypt.hash(data.password)
+        let user = User(name: data.name, username: data.username, password: password)
+        return user.save(on: req).map(to: Response.self) { user in
+            try req.authenticateSession(user)
+            return req.redirect(to: "/")
+        }
+    }
+    
 }
 
 struct IndexContext: Encodable {
@@ -294,4 +326,35 @@ struct LoginContext: Encodable {
 struct LoginPostData: Content {
     let username: String
     let password: String
+}
+
+struct RegisterContext: Encodable {
+    let title = "Register"
+    let message: String?
+    
+    init(message: String? = nil) {
+        self.message = message
+    }
+}
+
+struct RegisterData: Content {
+    let name: String
+    let username: String
+    let password: String
+    let confirmPassword: String
+}
+
+extension RegisterData: Validatable, Reflectable {
+    static func validations() throws -> Validations<RegisterData> {
+        var validations = Validations(RegisterData.self)
+        try validations.add(\.name, .ascii)
+        try validations.add(\.username, .alphanumeric && .count(3...))
+        try validations.add(\.password, .count(8...))
+        validations.add("passwords match") { model in
+            guard model.password == model.confirmPassword else {
+                throw BasicValidationError("passwords don't match")
+            }
+        }
+        return validations
+    }
 }
